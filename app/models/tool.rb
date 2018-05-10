27e519424ce004d852fc65d7285de2d04cdff51f
Casey Manning <caseykenjiro@yahoo.com>
2018-05-11 07:21:42 +1000

    fixed carrierwave add 2 uploaders gem fog-aws

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,5 @@
 class Tool < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  # mount_uploader :avatar, AvatarUploader
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,8 +4,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -17,7 +20,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
+  
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
