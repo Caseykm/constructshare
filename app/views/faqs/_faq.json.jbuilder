2f0404e0ab33a41e86296f0dc545d6eaf8a4c382
Casey Manning <caseykenjiro@yahoo.com>
2018-05-13 18:08:31 +1000

    created faq model,controller styled etc

---
+++ app/views/faqs/_faq.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! faq, :id, :leasing, :borrowing, :about, :created_at, :updated_at
+json.url faq_url(faq, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510110744) do
+ActiveRecord::Schema.define(version: 20180512220455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "faqs", force: :cascade do |t|
+    t.text "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "owners", force: :cascade do |t|
     t.string "surname"
